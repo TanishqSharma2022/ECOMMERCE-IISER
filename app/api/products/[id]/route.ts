ec91c172380f46952e69b7ec70db5ad073b54cdc
Tanishq Sharma <123730739+TanishqSharma2022@users.noreply.github.com>
2023-10-01 18:56:26 +0530

    disconeect added

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,10 +1,15 @@
 import dbConnect from "@/libs/mongodb";
 import product_model from "@/models/product_model"
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET(req:any, {params}:any){
     const {id} = params;
     await dbConnect();
     const product = await product_model.findById(id);
+
+    mongoose.disconnect()
     return NextResponse.json({ product }, {status: 200});
+
+
 }
--- app/api/products/iiserb/route.ts
+++ app/api/products/iiserb/route.ts
@@ -1,9 +1,11 @@
 import dbConnect from "@/libs/mongodb";
 import product_model from "@/models/product_model";
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET(){
     await dbConnect();
     const product = await product_model.find(({href: 'iiserb'}));
+    mongoose.disconnect()
     return NextResponse.json({ product });
 }
